5fa398ecdde8e7fdbcee45983f71fa5194b5a393
ryowakitazawa82 <ryowa.kitazawa@gmail.com>
2021-11-07 14:12:59 +0900

    finished section3

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var person = {
     name: 'Jack',
     age: 21
 };
-console.log(person.age);
+// console.log(person.age);
 /* 配列に型をつけるArraｙ型はこう使う */
 var fruits = ['apple', 'banana', 'greap'];
 // 配列に型をつけると、不必要なものが入ることを防げる
@@ -35,14 +35,84 @@ book[1] = 750; //　いける
 // 初期値は厳しくチェック。しかしpushはゆるい。そして参照するときは厳しい。
 /* Enumを使って、特定のまとまったグループのみを受け入れる列挙型を使う方法 */
 // enum = 列挙
+// enum coffeeSizes  {
+//     SHORT = 'SHORT',
+//     TALL = 'TALL',
+//     GRANDE = 'GRANDE',
+//     VENTI = 'VENTI'
+// }
+// 実は文字列にする必要ない
 var coffeeSizes;
 (function (coffeeSizes) {
-    coffeeSizes["SHORT"] = "SHORT";
-    coffeeSizes["TALL"] = "TALL";
-    coffeeSizes["GRANDE"] = "GRANDE";
-    coffeeSizes["VENTI"] = "VENTI";
+    coffeeSizes[coffeeSizes["SHORT"] = 0] = "SHORT";
+    coffeeSizes[coffeeSizes["TALL"] = 1] = "TALL";
+    coffeeSizes[coffeeSizes["GRANDE"] = 2] = "GRANDE";
+    coffeeSizes[coffeeSizes["VENTI"] = 3] = "VENTI";
 })(coffeeSizes || (coffeeSizes = {}));
 var coffee = {
     hot: true,
-    size: coffeeSizes.TALL
+    size: coffeeSizes.SHORT
 };
+/* any型 */
+// anyはなんでも入る
+// anyはなるべく使わない、カオスになる
+/* Union型を使って複数の型を使用する方法 */
+var unionType = 10; // numberも入れたいし、stringもいれたい、、とき
+unionType = 'hello'; // エラー起きない！
+var unionTypes = [21, 'hello'];
+/* Literal型を使って特定の値のみ取り扱う方法 */
+var apple = 'apple'; // 完全に決まった方のみを扱う
+var banana = 'banana'; // constにすると上と同じに
+var cloth = {
+    color: 'white',
+    size: 'midium'
+};
+/* 関数に型を適応させる方法 */
+function add(num1, num2) {
+    return num1 + num2;
+}
+//パラメータの後ろに戻り値の型をつける
+// 型推論は？戻り値は効くが、引数は効かない
+// 関数が複雑になった時に戻り値に型注釈してあると、わかりやすいからつけるべきである
+add(3, 2);
+/* 関数の戻り値にvoid型を使う方法 */
+function sayHello() {
+    // console.log('Hello')
+}
+// console.log(sayHello());
+// consoleの結果
+// Hello
+// undefined
+/* 関数を使って、特定の関数のみを代入できる変数を作る */
+var anotherAdd = add;
+// const anotherAdd:(n1: number, n2: number)=> number  = function add(num1: number, num2: number): number {
+//     return num1 + num2
+// };
+//無名関数にすることも可能
+//アロー関数は？
+// const doubleNumber = (number: number): number => number *2; //アロー関数なのに右側が長い！！
+var doubleNumber = function (number) { return number * 2; }; //左につけりゃいい
+/* callback関数の型はこう書く */ //※理解浅いから要復習
+function doubleAndHandle(num, cb) {
+    var doubleNum = cb(num * 2);
+    // console.log(doubleNum);
+}
+doubleAndHandle(21, function (doubleNum) {
+    return doubleNum;
+});
+/* unKnown型を使って、柔軟でanyよりも厳しい型を使用する方法 */ //※理解浅いから要復習
+var unKnownInput;
+var anyInput;
+unKnownInput = 'hello world';
+unKnownInput = 21;
+unKnownInput = true;
+// unknown型はanyと同じくなんでも入れられる
+// じゃあどこが違う？
+var text;
+// text = unKnownInput; //エラー出る
+text = anyInput; //anyだとエラー出ないのに
+/* never型を使って、起こり得ない値の方を使用する方法 */
+function error(message) {
+    throw new Error(message);
+}
+console.log(error('aa'));
--- index.ts
+++ index.ts
@@ -36,7 +36,7 @@ const person : {
     age: 21,
 }
 
-console.log(person.age);
+// console.log(person.age);
 
 
 /* 配列に型をつけるArraｙ型はこう使う */
@@ -77,3 +77,123 @@ const coffee = {
     hot: true,
     size: coffeeSizes.SHORT,
 }
+
+
+/* any型 */
+
+// anyはなんでも入る
+// anyはなるべく使わない、カオスになる
+
+
+/* Union型を使って複数の型を使用する方法 */
+
+let unionType: number | string = 10; // numberも入れたいし、stringもいれたい、、とき
+unionType = 'hello'; // エラー起きない！
+
+let unionTypes: (number | string) [] = [21, 'hello']
+
+
+/* Literal型を使って特定の値のみ取り扱う方法 */
+
+let apple: 'apple' = 'apple'; // 完全に決まった方のみを扱う
+const banana = 'banana'; // constにすると上と同じに
+
+//Literal型っていつ使うの？ Union型とLiteral型を組み合わせることでenum型と非常によく似た使い方ができる
+// let clothSize: 'small' | 'midium' | 'large' = 'large';
+
+// const cloth: {
+//     color: string;
+//     size: 'small' | 'midium' | 'large'
+// } = {
+//     color: 'white',
+//     size: 'midium'
+// }
+
+//enumとどっち使うのがいいの？
+// 数が2-3のときはUnion型とLiteral型を組み合わせのがスッキリしててよい
+
+
+/* typeエイリアスを使って複雑な型を変数のように扱う */
+
+type clothSize = 'small' | 'midium' | 'large'
+const cloth: {
+    color: string;
+    size: clothSize //ここで使える！
+} = {
+    color: 'white',
+    size: 'midium'
+}
+
+
+/* 関数に型を適応させる方法 */
+
+function add(num1: number, num2: number): number {
+    return num1 + num2
+}
+//パラメータの後ろに戻り値の型をつける
+// 型推論は？戻り値は効くが、引数は効かない
+// 関数が複雑になった時に戻り値に型注釈してあると、わかりやすいからつけるべきである
+add(3,2);
+
+
+/* 関数の戻り値にvoid型を使う方法 */
+
+function sayHello(): void {
+    // console.log('Hello')
+}
+
+// console.log(sayHello());
+
+// consoleの結果
+// Hello
+// undefined
+
+
+/* 関数を使って、特定の関数のみを代入できる変数を作る */
+
+const anotherAdd:(n1: number, n2: number)=> number  = add;
+// const anotherAdd:(n1: number, n2: number)=> number  = function add(num1: number, num2: number): number {
+//     return num1 + num2
+// };
+//無名関数にすることも可能
+
+//アロー関数は？
+// const doubleNumber = (number: number): number => number *2; //アロー関数なのに右側が長い！！
+const doubleNumber: (num:number) => number = number => number *2; //左につけりゃいい
+
+
+/* callback関数の型はこう書く */ //※理解浅いから要復習
+
+function doubleAndHandle(num: number, cb: (num: number) => number): void {
+    const doubleNum = cb(num * 2);
+    // console.log(doubleNum);
+}
+
+doubleAndHandle(21, doubleNum => {
+    return doubleNum
+});
+
+
+/* unKnown型を使って、柔軟でanyよりも厳しい型を使用する方法 */ //※理解浅いから要復習
+
+let unKnownInput: unknown;
+let anyInput: any;
+
+unKnownInput = 'hello world';
+unKnownInput = 21;
+unKnownInput = true;
+// unknown型はanyと同じくなんでも入れられる
+// じゃあどこが違う？
+let text: string;
+// text = unKnownInput; //エラー出る
+text = anyInput; //anyだとエラー出ないのに
+
+
+/* never型を使って、起こり得ない値の方を使用する方法 */
+
+function error(message: string): never {
+    throw new Error(message);
+}
+
+console.log(error('aa'));
+
