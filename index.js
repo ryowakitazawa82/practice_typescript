5312862f7608de882b63cb1a697953d783325aa2
ryowakitazawa82 <ryowa.kitazawa@gmail.com>
2021-11-07 12:20:58 +0900

    -20

--- index.js
+++ index.js
@@ -4,76 +4,45 @@
 //     GRANDE = 'GRANDE',
 //     VENTI = 'VENTI'
 // }
-
 // const coffee = {
 //     hot: true,
 //     size: CoffeeSize.TALL,
 // }
-
 /* boolean型とnumber型とstring型を使う方法 */
-
-let hasValue: boolean = true;
-let count: number = 10;
-let float: number = 3.14;
-let negative: number = -0.12;
+var hasValue = true;
+var count = 10;
+var float = 3.14;
+var negative = -0.12;
 // javascript.typescriptは少数・マイナスもnumber型でいける
-let single: string ='hello';
-
-
+var single = 'hello';
 /* 型注釈と型推論 */
-
 // 基本的には、型推論でやる（いちいち書くの大変）
 // 型推論ができない時のみ型注釈で記述（例）any型など
-
-
 /* オブジェクトに型をつける方法 */
-
-const person : {
-    name: string;
-    age: number;
-} = {
+var person = {
     name: 'Jack',
-    age: 21,
-}
-
+    age: 21
+};
 console.log(person.age);
-
-
 /* 配列に型をつけるArraｙ型はこう使う */
-
-const fruits: string[] = ['apple', 'banana', 'greap']
+var fruits = ['apple', 'banana', 'greap'];
 // 配列に型をつけると、不必要なものが入ることを防げる
-const fruit = fruits[0];
-
-
+var fruit = fruits[0];
 /* Tuple型を使用して、決まった内容の配列を作る方法 */
-
-const book: [string, number, boolean] = ['business', 1500, false];
+var book = ['business', 1500, false];
 book[1] = 750; //　いける
 // book[1] = 'hello';　エラー起きる
 // 初期値は厳しくチェック。しかしpushはゆるい。そして参照するときは厳しい。
-
-
 /* Enumを使って、特定のまとまったグループのみを受け入れる列挙型を使う方法 */
-
 // enum = 列挙
-
-// enum coffeeSizes  {
-//     SHORT = 'SHORT',
-//     TALL = 'TALL',
-//     GRANDE = 'GRANDE',
-//     VENTI = 'VENTI'
-// }
-// 実は文字列にする必要ない
-
-enum coffeeSizes  {
-    SHORT,
-    TALL,
-    GRANDE,
-    VENTI,
-}
-
-const coffee = {
+var coffeeSizes;
+(function (coffeeSizes) {
+    coffeeSizes["SHORT"] = "SHORT";
+    coffeeSizes["TALL"] = "TALL";
+    coffeeSizes["GRANDE"] = "GRANDE";
+    coffeeSizes["VENTI"] = "VENTI";
+})(coffeeSizes || (coffeeSizes = {}));
+var coffee = {
     hot: true,
-    size: coffeeSizes.SHORT,
-}
+    size: coffeeSizes.TALL
+};
